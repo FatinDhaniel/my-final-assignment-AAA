10bee23a640848ce74025a5aa545e2003eb32406
FatinDhaniel <58180586+FatinDhaniel@users.noreply.github.com>
2022-06-09 15:03:12 +0800

    Update project.py

--- project.py
+++ project.py
@@ -8,22 +8,12 @@ st.write("""
 This app predicts the **Iris flower** type!
 """)
 
-st.sidebar.header('User Input Parameters') 
+st.sidebar.header('User Input Parameters')
+st.sidebar.header('Upload file')
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
-     # To read file as bytes:
-   bytes_data = uploaded_file.getvalue()
-   st.write(bytes_data)
-
-     # To convert to a string based IO:
-   stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-   st.write(stringio)
-
-     # To read file as string:
-   string_data = stringio.read()
-   st.write(string_data)
-
-     # Can be used wherever a "file-like" object is accepted:
+     
+    # Can be used wherever a "file-like" object is accepted:
    dataframe = pd.read_csv(uploaded_file)
    st.write(dataframe)
 
