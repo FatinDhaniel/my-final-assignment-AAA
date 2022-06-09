a3356a6b1a45faaea9f1a94df93051aadb6ef72d
FatinDhaniel <58180586+FatinDhaniel@users.noreply.github.com>
2022-06-09 16:30:36 +0800

    Update project.py

--- project.py
+++ project.py
@@ -10,7 +10,7 @@ This app predicts the **Iris flower** type!
 from PIL import Image
 image = Image.open('iris.jpg')
 
-st.image(image, caption='Iris flower illustration)
+st.image(image, caption='Iris flower illustration')
 
 st.sidebar.header('User Input Parameters')
 def user_input_features():
