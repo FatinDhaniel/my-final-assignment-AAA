e4835142c43814b50dbe9571a627f29f9e43605c
FatinDhaniel <58180586+FatinDhaniel@users.noreply.github.com>
2022-06-09 15:11:36 +0800

    Update project.py

--- project.py
+++ project.py
@@ -14,7 +14,7 @@ uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
      
     # Can be used wherever a "file-like" object is accepted:
-   dataframe = pd.read_csv(uploaded_file)
+   dataframe = st.sidebar.pd.read_csv(uploaded_file)
    st.write(dataframe)
 
 def user_input_features():
